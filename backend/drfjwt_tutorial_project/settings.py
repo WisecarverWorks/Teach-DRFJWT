cf9a81df2a980670e80ff958d246e503443c9b0f
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-06-30 10:23:36 -0500

    docs: Settings file updated for Rest Framework + Urls file updated in project File.

--- backend/drfjwt_tutorial_project/settings.py
+++ backend/drfjwt_tutorial_project/settings.py
@@ -15,17 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -36,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ## Local installs
+    'rest_framework',
     'authentication.apps.AuthenticationConfig', 
     'packages.apps.PackagesConfig', 
 ]
@@ -69,17 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'drfjwt_tutorial_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -122,3 +109,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
+try:
+    from drfjwt_tutorial_project.local_settings import *
+except ImportError:
+    pass 
